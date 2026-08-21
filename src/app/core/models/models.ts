ef8221e372bc41614770c9df24759839ae2bf817
PAPPA-Ikan <pappaunion@gmail.com>
2026-08-21 18:46:12 +0700

    new dashboard

---
+++ src/app/core/models/models.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-models',
+  styleUrl: './models.css',
+  templateUrl: './models.html',
+})
+export class Models {}
---
+++ src/app/shared/ui/ui.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-ui',
+  styleUrl: './ui.css',
+  templateUrl: './ui.html',
+})
+export class Ui {}
