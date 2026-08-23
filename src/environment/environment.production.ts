9d16834f4eec28cbf686aa79369422b82f11aed1
PAPPA-Ikan <pappaunion@gmail.com>
2026-08-23 20:26:17 +0700

    remove statis data

---
+++ src/environment/environment.production.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+
+  supabaseUrl: 'https://ignmojzeinbyebmkcask.supabase.co/',
+
+  supabasePublishableKey: 'sb_publishable_9TBWnHiHJuYReP_oLHfgXQ_iOLqAnT4',
+};
---
+++ src/environment/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+
+  supabaseUrl: 'https://ignmojzeinbyebmkcask.supabase.co/',
+
+  supabasePublishableKey: 'sb_publishable_9TBWnHiHJuYReP_oLHfgXQ_iOLqAnT4',
+};
