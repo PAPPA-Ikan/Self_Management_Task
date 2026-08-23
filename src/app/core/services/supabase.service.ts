9953b75f2615b3b75b8d4b723bd14fc1e94a7192
PAPPA-Ikan <pappaunion@gmail.com>
2026-08-23 20:54:38 +0700

    fixing over typing import

--- src/app/core/services/supabase.service.ts
+++ src/app/core/services/supabase.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-import {
-  createClient,
-  SupabaseClient,
-} from '@supabase/supabase-js';
-
-import { environment } from '../../../environment/environment.ts';
+import { environment } from '../../../environment/environment';
 
 @Injectable({
   providedIn: 'root',
